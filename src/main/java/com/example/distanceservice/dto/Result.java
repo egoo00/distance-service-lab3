02a28a5d081d5d6f1e247f60e5060b7bc0a9883a
egoo00 <egooo914@gmail.com>
2025-06-01 10:16:35 +0300

    Update Result.java

--- src/main/java/com/example/distanceservice/dto/Result.java
+++ src/main/java/com/example/distanceservice/dto/Result.java
@@ -1,16 +1,8 @@
 package com.example.distanceservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Result {
-    @JsonProperty("geometry")
     private Geometry geometry;
 
-    public Geometry getGeometry() {
-        return geometry;
-    }
-
-    public void setGeometry(Geometry geometry) {
-        this.geometry = geometry;
-    }
+    public Geometry getGeometry() { return geometry; }
+    public void setGeometry(Geometry geometry) { this.geometry = geometry; }
 }
