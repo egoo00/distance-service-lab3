6c4ef0da4d6bf6a6f153ed05614cd55d372c7181
egoo00 <egooo914@gmail.com>
2025-06-02 16:15:29 +0300

    Update CountryService.java

--- src/main/java/com/example/distanceservice/service/CountryService.java
+++ src/main/java/com/example/distanceservice/service/CountryService.java
@@ -1,5 +1,6 @@
 package com.example.distanceservice.service;
 
+import com.example.distanceservice.cache.SimpleCache;
 import com.example.distanceservice.entity.Country;
 import com.example.distanceservice.repository.CountryRepository;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,11 @@ import java.util.Optional;
 @Service
 public class CountryService {
     private final CountryRepository countryRepository;
+    private final SimpleCache simpleCache;
 
-    public CountryService(CountryRepository countryRepository) {
+    public CountryService(CountryRepository countryRepository, SimpleCache simpleCache) {
         this.countryRepository = countryRepository;
+        this.simpleCache = simpleCache;
     }
 
     public List<Country> getAllCountries() {
@@ -24,17 +27,22 @@ public class CountryService {
     }
 
     public Country saveCountry(Country country) {
-        return countryRepository.save(country);
+        Country savedCountry = countryRepository.save(country);
+        simpleCache.put("countries_all", countryRepository.findAll());
+        return savedCountry;
     }
 
     public void deleteCountry(Long id) {
         countryRepository.deleteById(id);
+        simpleCache.put("countries_all", countryRepository.findAll());
     }
 
     public Country updateCountry(Long id, Country updatedCountry) {
-        return countryRepository.findById(id).map(country -> {
+        Country updated = countryRepository.findById(id).map(country -> {
             country.setName(updatedCountry.getName());
             return countryRepository.save(country);
         }).orElseThrow(() -> new RuntimeException("Country not found with id: " + id));
+        simpleCache.put("countries_all", countryRepository.findAll());
+        return updated;
     }
 }
