e163d79df2c79923c68ab9e6f8e41737faca7538
egoo00 <egooo914@gmail.com>
2025-06-02 08:41:07 +0300

    Update DistanceService.java

--- src/main/java/com/example/distanceservice/service/DistanceService.java
+++ src/main/java/com/example/distanceservice/service/DistanceService.java
@@ -1,5 +1,6 @@
 package com.example.distanceservice.service;
 
+import com.example.distanceservice.cache.SimpleCache;
 import com.example.distanceservice.dto.DistanceResponse;
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.exception.CityNotFoundException;
@@ -10,29 +11,30 @@ import org.springframework.stereotype.Service;
 public class DistanceService {
     private final CityRepository cityRepository;
     private final GeocodingService geocodingService;
+    private final SimpleCache simpleCache;
 
-    public DistanceService(CityRepository cityRepository, GeocodingService geocodingService) {
+    public DistanceService(CityRepository cityRepository, GeocodingService geocodingService, SimpleCache simpleCache) {
         this.cityRepository = cityRepository;
         this.geocodingService = geocodingService;
+        this.simpleCache = simpleCache;
     }
 
     public DistanceResponse calculateDistance(String city1, String city2) {
-        City c1 = cityRepository.findByName(city1).orElse(null);
-        if (c1 == null) {
-            c1 = geocodingService.getCity(city1);
-            
-            if (c1 == null) {
-                return new DistanceResponse(city1, city2, -1, "km"); 
-            }
+        String cacheKey = "distance_" + city1 + "_" + city2;
+        DistanceResponse cachedResponse = (DistanceResponse) simpleCache.get(cacheKey);
+
+        if (cachedResponse != null) {
+            return cachedResponse;
         }
 
-        City c2 = cityRepository.findByName(city2).orElse(null);
+        City c1 = cityRepository.findByName(city1);
+        if (c1 == null) {
+            c1 = geocodingService.getCity(city1);
+        }
+
+        City c2 = cityRepository.findByName(city2);
         if (c2 == null) {
             c2 = geocodingService.getCity(city2);
-            
-            if (c2 == null) {
-                return new DistanceResponse(city1, city2, -1, "km"); 
-            }
         }
 
         double distance = calculateHaversine(
@@ -40,7 +42,9 @@ public class DistanceService {
                 c2.getLatitude(), c2.getLongitude()
         );
 
-        return new DistanceResponse(city1, city2, distance, "km");
+        DistanceResponse response = new DistanceResponse(city1, city2, distance, "km");
+        simpleCache.put(cacheKey, response);
+        return response;
     }
 
     private double calculateHaversine(double lat1, double lon1, double lat2, double lon2) {
