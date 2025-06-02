2ee76429ad5796eef0b92f7c69dcc4656b502437
egoo00 <egooo914@gmail.com>
2025-06-02 08:26:24 +0300

    Update Geometry.java

--- src/main/java/com/example/distanceservice/dto/Geometry.java
+++ src/main/java/com/example/distanceservice/dto/Geometry.java
@@ -1,27 +1,11 @@
 package com.example.distanceservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Geometry {
     private double lat;
-
-    @JsonProperty("lng")
     private double lng;
 
-  
-    public double getLat() {
-        return lat;
-    }
-
-    public double getLng() {
-        return lng;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public void setLng(double lng) {
-        this.lng = lng;
-    }
+    public double getLat() { return lat; }
+    public void setLat(double lat) { this.lat = lat; }
+    public double getLng() { return lng; }
+    public void setLng(double lng) { this.lng = lng; }
 }
