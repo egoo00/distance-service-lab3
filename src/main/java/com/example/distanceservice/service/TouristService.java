ee9a812110c099ee280c2cd9d9522c2de40d90f3
egoo00 <egooo914@gmail.com>
2025-06-02 16:16:20 +0300

    Update TouristService.java

--- src/main/java/com/example/distanceservice/service/TouristService.java
+++ src/main/java/com/example/distanceservice/service/TouristService.java
@@ -1,5 +1,6 @@
 package com.example.distanceservice.service;
 
+import com.example.distanceservice.cache.SimpleCache;
 import com.example.distanceservice.entity.Tourist;
 import com.example.distanceservice.repository.TouristRepository;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,11 @@ import java.util.Optional;
 @Service
 public class TouristService {
     private final TouristRepository touristRepository;
+    private final SimpleCache simpleCache;
 
-    public TouristService(TouristRepository touristRepository) {
+    public TouristService(TouristRepository touristRepository, SimpleCache simpleCache) {
         this.touristRepository = touristRepository;
+        this.simpleCache = simpleCache;
     }
 
     public List<Tourist> getAllTourists() {
@@ -24,18 +27,23 @@ public class TouristService {
     }
 
     public Tourist saveTourist(Tourist tourist) {
-        return touristRepository.save(tourist);
+        Tourist savedTourist = touristRepository.save(tourist);
+        simpleCache.put("tourists_all", touristRepository.findAll());
+        return savedTourist;
     }
 
     public void deleteTourist(Long id) {
         touristRepository.deleteById(id);
+        simpleCache.put("tourists_all", touristRepository.findAll());
     }
 
     public Tourist updateTourist(Long id, Tourist updatedTourist) {
-        return touristRepository.findById(id).map(tourist -> {
+        Tourist updated = touristRepository.findById(id).map(tourist -> {
             tourist.setName(updatedTourist.getName());
             tourist.setVisitedCities(updatedTourist.getVisitedCities());
             return touristRepository.save(tourist);
         }).orElseThrow(() -> new RuntimeException("Tourist not found with id: " + id));
+        simpleCache.put("tourists_all", touristRepository.findAll());
+        return updated;
     }
 }
