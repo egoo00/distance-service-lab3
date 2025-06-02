cc51202575e29a1cb40cf6442e748a10cf44b416
egoo00 <egooo914@gmail.com>
2025-06-02 16:14:59 +0300

    Update CityService.java

--- src/main/java/com/example/distanceservice/service/CityService.java
+++ src/main/java/com/example/distanceservice/service/CityService.java
@@ -1,5 +1,6 @@
 package com.example.distanceservice.service;
 
+import com.example.distanceservice.cache.SimpleCache;
 import com.example.distanceservice.entity.City;
 import com.example.distanceservice.repository.CityRepository;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,11 @@ import java.util.Optional;
 @Service
 public class CityService {
     private final CityRepository cityRepository;
+    private final SimpleCache simpleCache;
 
-    public CityService(CityRepository cityRepository) {
+    public CityService(CityRepository cityRepository, SimpleCache simpleCache) {
         this.cityRepository = cityRepository;
+        this.simpleCache = simpleCache;
     }
 
     public List<City> getAllCities() {
@@ -24,19 +27,24 @@ public class CityService {
     }
 
     public City saveCity(City city) {
-        return cityRepository.save(city);
+        City savedCity = cityRepository.save(city);
+        simpleCache.put("cities_all", cityRepository.findAll());
+        return savedCity;
     }
 
     public void deleteCity(Long id) {
         cityRepository.deleteById(id);
+        simpleCache.put("cities_all", cityRepository.findAll());
     }
 
     public City updateCity(Long id, City updatedCity) {
-        return cityRepository.findById(id).map(city -> {
+        City updated = cityRepository.findById(id).map(city -> {
             city.setName(updatedCity.getName());
             city.setLatitude(updatedCity.getLatitude());
             city.setLongitude(updatedCity.getLongitude());
             return cityRepository.save(city);
         }).orElseThrow(() -> new RuntimeException("City not found with id: " + id));
+        simpleCache.put("cities_all", cityRepository.findAll());
+        return updated;
     }
 }
